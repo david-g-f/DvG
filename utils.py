b84db0c562dca997489d87d5070a8bc03fd1ac5d
David <dgf2001@hw.ac.uk>
2026-01-18 13:08:14 +0400

    Changed utils to use logging instead of print statements

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 import torch
 import gc
+import logging
 
 # Function that monitors current VRAM usage without clearing cache
 def checkVRAM():
     vramUsed = torch.cuda.memory_allocated() / (1024 ** 3)
     vramTotal = torch.cuda.get_device_properties(0).total_memory / (1024 ** 3)
-    print(f"--- VRAM Monitor")
-    print(f" -- Used VRAM: {vramUsed:.2f} / {vramTotal:.2f} GB")
-    print("----")
+    logging.info("--- VRAM Monitor")
+    logging.info(" -- Used VRAM: {vramUsed:.2f} / {vramTotal:.2f} GB")
+    logging.info("----")
     return vramUsed, vramTotal 
 
 # Function that serves to offload the SLM after processing to then load the LLM afterwards -- AKA memory swapping
@@ -21,8 +22,8 @@ def clearVRAM():
     vramFinal = torch.cuda.memory_allocated() / (1024 ** 3)
     vramTotal = torch.cuda.get_device_properties(0).total_memory / (1024 ** 3)
 
-    print(f"--- Emptying VRAM")
-    print(f" -- Memory Cleared: {vramAvailable - vramFinal:.2f} GB")
-    print(f" -- Current Occupied Memory: {vramFinal:.2f} GB")
-    print(f" -- Free Memory: {vramTotal - vramFinal:.2f} / {vramTotal:.2f} GB")
-    print("----")
+    logging.info(f"--- Emptying VRAM")
+    logging.info(f" -- Memory Cleared: {vramAvailable - vramFinal:.2f} GB")
+    logging.info(f" -- Current Occupied Memory: {vramFinal:.2f} GB")
+    logging.info(f" -- Free Memory: {vramTotal - vramFinal:.2f} / {vramTotal:.2f} GB")
+    logging.info("----")
