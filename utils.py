fee1f1bbfc849ce8a17d23fe7c2312cb7296bca4
David <dgf2001@hw.ac.uk>
2026-01-18 13:08:45 +0400

    Changed unnecessary logging

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def checkVRAM():
     vramTotal = torch.cuda.get_device_properties(0).total_memory / (1024 ** 3)
     logging.info("--- VRAM Monitor")
     logging.info(" -- Used VRAM: {vramUsed:.2f} / {vramTotal:.2f} GB")
-    logging.info("----")
+    print("----")
     return vramUsed, vramTotal 
 
 # Function that serves to offload the SLM after processing to then load the LLM afterwards -- AKA memory swapping
@@ -26,4 +26,4 @@ def clearVRAM():
     logging.info(f" -- Memory Cleared: {vramAvailable - vramFinal:.2f} GB")
     logging.info(f" -- Current Occupied Memory: {vramFinal:.2f} GB")
     logging.info(f" -- Free Memory: {vramTotal - vramFinal:.2f} / {vramTotal:.2f} GB")
-    logging.info("----")
+    print("----")
